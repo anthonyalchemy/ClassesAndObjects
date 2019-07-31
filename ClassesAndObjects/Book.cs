5863b9266c228369eb1b17597e1ddc2be456a299
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 14:05:10 +0100

    Add project files.

---
+++ ClassesAndObjects/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Book
+    {
+        public string title; // class attributes 
+        public string author;
+        public int pages;
+
+        //now we can go to other prgram and create a book 
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //classes and ojects allow you to create custom data types
+            //classes model real world  entities 
+            /* Go to solution explorer 
+             * Click on program 
+             * add class 
+             * rename 
+             */
+
+            //object will be created here which is an instance of a class 
+
+            Book book1 = new Book(); //book object
+            book1.title = "Harry Potter";
+            book1.author = "J.K.Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book(); //book object
+            book2.title = "Lord of the rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book2.pages);
+             
+
+            Console.ReadLine();
+
+        }
+    }
+}
